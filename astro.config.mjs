e367922eac4477c5e823db87c688262319dfd28b
Nina Kin <matikin9@gmail.com>
2024-04-14 18:21:08 -0700

    init Astro

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: https://matikin9.github.io',
+    base: 'la-restaurant-map',
+});
