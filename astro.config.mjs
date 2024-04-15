2818bdda906a8261f4460a1effa58d7cf70c8cc8
Nina Kin <matikin9@gmail.com>
2024-04-14 18:26:52 -0700

    typo in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: https://matikin9.github.io',
+    site: 'https://matikin9.github.io',
     base: 'la-restaurant-map',
 });
